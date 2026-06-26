7faccf1cb5320f532a757c7e35b1ed62085f6167
GomolemoDizamuhupe <ST10491441@rcconnect.edu.za>
2026-06-26 11:54:06 +0200

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,8 +1,10 @@
 /*Creating db*/
 Create database prog_task_1;
 
+/*Using the db*/
 USE [prog_task_1];
 
+/*Creating the table*/
 Create table tasks(
 task_id int primary key identity(1,1),
 task_name varchar(225),
@@ -11,4 +13,5 @@ task_dueDate varchar(20),
 task_status varchar(20),
 );
 
+/*Selecting the table*/
 Select * from tasks;
